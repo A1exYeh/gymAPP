67950b39eafefd3c2a34fc7b36288ea5507c8957
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2024-09-17 19:19:35 -0700

    use path.join for express static folder directory

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 //we are returning files from the public directory only
 //tell Render to use public folder to serve static pages
-app.use(express.static((__dirname + 'public'));
+app.use(express.static((path.join(__dirname, 'public'))));
 
 //setup get handler for index.html for when server starts
 app.get ('/', (req, res) => {
