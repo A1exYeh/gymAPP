2055f8e690b50b24777ca3b5d8cf441afef2edeb
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2024-10-21 14:18:17 -0700

    Add prototype for bootstrap dashboard & nav

--- public/dash.js
+++ public/dash.js
@@ -10,7 +10,8 @@ const logOutButton = document.getElementById('logOutButton');
 const addExercisePopup = document.getElementById('addExercisePopup');
 const addExerciseForm = document.getElementById('addExerciseForm');
 const closeExerciseForm = document.getElementById('closeExerciseForm');
-
+const navTogglerLogo = document.getElementById('navTogglerLogo');
+let navLogoToggle = false;
 //updates an exercise with data from user
 function updateExercise(exerciseName, newWeight) {
    fetch('/updateExercise', {
@@ -132,9 +133,26 @@ function createExerciseCard(exercise) {
 
    return card;
 }
+
+function navLogoToggler() {
+   if (!navLogoToggle) {
+      navTogglerLogo.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"  class="bi bi-list-nested" viewBox="0 0 16 16">' + '<path fill-rule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5"/></svg>';
+      navLogoToggle = true;
+   } else {
+      navTogglerLogo.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"  class="bi bi-list" viewBox="0 0 16 16">' + '<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>';
+      navLogoToggle = false;
+   }
+}
 // get and populate the exercise list with exercise cards made up of user exercises
 // on page refresh
 document.addEventListener('DOMContentLoaded', function (e) {
+   //set up nav toggler hover
+   
+
+   navTogglerLogo.addEventListener('click', function() {
+      navLogoToggler();
+   });
+
    //hide our add exercise popup
    addExercisePopup.style.display = 'none';
    const exerciseList = document.getElementById('exerciseList');
