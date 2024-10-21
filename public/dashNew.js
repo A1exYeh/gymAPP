e785683f38fb66ea3cd3f47dac7db510ee77f3aa
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2024-10-21 16:01:49 -0700

    Add copies of node.js server for testing

---
+++ public/dashNew.js
@@ -0,0 +1,299 @@
+//check for existence of dashboard header to verify that we are in dashboard.html
+const dashboard = document.getElementById('dashHeader');
+//test variables for user input 
+const usernameID = document.getElementById('usernameID');
+const lastGymVisit = document.getElementById('lastGymVisit');
+const userInput = document.getElementById('userInput');
+const addExerciseButton = document.getElementById('addExerciseButton');
+const updateGymVisitButton = document.getElementById('addVisitDateButton');
+const logOutButton = document.getElementById('logOutButton');
+const addExercisePopup = document.getElementById('addExercisePopup');
+const addExerciseForm = document.getElementById('addExerciseForm');
+const closeExerciseForm = document.getElementById('closeExerciseForm');
+const navTogglerLogo = document.getElementById('navTogglerLogo');
+let navLogoToggle = false;
+//updates an exercise with data from user
+function updateExercise(exerciseName, newWeight) {
+   fetch('/updateExercise', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+            exerciseName,
+            weight: newWeight
+         }),
+      })
+      .then(response => response.json())
+      .then(data => {
+         if (data.success) {
+            console.log(data.message);
+
+            const exerciseCard = document.querySelector(`#exerciseCard #exerciseTitle[data-name="${exerciseName}"]`).closest('#exerciseCard');
+
+            // console.log(exerciseCard); idk how to check if we are selecting the right card
+            if (exerciseCard) {
+               const lastWeightElement = exerciseCard.querySelector('#exerciseLastWeight');
+               if (lastWeightElement) {
+                  lastWeightElement.innerHTML = `${newWeight} <span>LBS</span>`;
+               }
+            }
+         } else {
+            console.log("Unable to Update Exercise.");
+         }
+      })
+      .catch((error) => {
+         console.error('Error: ', error);
+      })
+}
+
+function deleteExercise(exerciseName) {
+   fetch('/deleteExercise', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+         exerciseDeleteName: exerciseName //stringify the title of this exercise card as a json
+      })
+   })
+   .then (response => response.json())
+   .then (data => {
+      console.log(JSON.stringify(data));
+      if (data.success) {
+         console.log(data.message);
+         const exerciseCard = document.querySelector(`#exerciseCard #exerciseTitle[data-name="${exerciseName}"]`).closest('#exerciseCard');
+         if (exerciseCard) {
+            exerciseCard.remove();
+         }
+      } else {
+         console.log("Error deleting exercise.");
+      }
+   })
+}
+
+//creates and populates an exercise card given an exercise object
+function createExerciseCard(exercise) {
+   const card = document.createElement('li');
+   card.classList.add('list-group-item');
+   card.innerHTML = `
+      <div id="exerciseCard">
+         <h3 id="exerciseTitle" data-name="${exercise.name}">${exercise.name}</h3>
+         <div id="exerciseLastWeight">${exercise.lastWeight}<span> LBs</span></div>
+         <input type="number" id="newExerciseWeight" placeholder="New weight" min="1">
+         <button id="updateExercise">
+            <i class="bi bi-arrow-clockwise"></i>
+         </button>
+         <button id="deleteExercise">
+            <i class="bi bi-trash-fill"></i>
+         </button>
+      </div>
+      `;
+
+   //in the previous code in this function we create a div with the id exerciseCard
+   //then we populate it with the exercise card html to which we also update the values with
+   //the data we pull from our db
+   //this const below selects the updateExercise id button within our newly created card
+   const updateExerciseButton = card.querySelector('#updateExercise');
+   //for the update exercise button in the card we just made, we attach an event listener
+   if (updateExerciseButton) {
+      //when a user click this update exercise button, if there is a new weight inputted 
+      //we will update the exercise in our db and the page will load it accordingly
+      updateExerciseButton.addEventListener('click', function () {
+         const newWeightInput = card.querySelector('#newExerciseWeight');
+         if (newWeightInput) {
+            const newWeight = newWeightInput.value;
+            if (newWeight > 0) {
+               updateExercise(exercise.name, newWeight);
+            } else {
+               console.error(`New weight input invalid ${exercise.name}`);
+               alert(`New weight for ${exercise.name} must be more than 0.`);
+            }
+         } else {
+            console.error(`New weight input not found for ${exercise.name}`);
+         }
+      });
+   } else {
+      console.error(`Update button not found for ${exercise.name}`);
+   }
+
+   const deleteExerciseButton = card.querySelector('#deleteExercise');
+   if (deleteExerciseButton) {
+      //when a user click this update exercise button, if there is a new weight inputted 
+      //we will update the exercise in our db and the page will load it accordingly
+      deleteExerciseButton.addEventListener('click', function () {
+         const exerciseName = card.querySelector('#exerciseTitle');
+         if (exerciseName) {
+            deleteExercise(exercise.name);
+         } else {
+            console.error(`Could not get name for: ${exercise.name}`);
+         }
+      });
+   } else {
+      console.error(`Delete button not found for ${exercise.name}`);
+   }
+
+   return card;
+}
+
+function navLogoToggler() {
+   if (!navLogoToggle) {
+      navTogglerLogo.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"  class="bi bi-list-nested" viewBox="0 0 16 16">' + '<path fill-rule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5"/></svg>';
+      navLogoToggle = true;
+   } else {
+      navTogglerLogo.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"  class="bi bi-list" viewBox="0 0 16 16">' + '<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg>';
+      navLogoToggle = false;
+   }
+}
+// get and populate the exercise list with exercise cards made up of user exercises
+// on page refresh
+document.addEventListener('DOMContentLoaded', function (e) {
+   //set up nav toggler hover
+   
+
+   navTogglerLogo.addEventListener('click', function() {
+      navLogoToggler();
+   });
+
+   //hide our add exercise popup
+   addExercisePopup.style.display = 'none';
+   const exerciseList = document.getElementById('exerciseList');
+   fetch('/sessionData', {
+         method: 'GET'
+      })
+      .then(response => response.json())
+      .then(data => {
+         console.log(JSON.stringify(data, null, 2));
+         console.log("Session Data: ", data);
+         if (data.username) {
+            usernameID.textContent = data.username; //set the username
+         }
+
+         if (data.lastGymVisit) {
+            console.log("last gym visit: ", data.lastGymVisit);
+            lastGymVisit.textContent = new Date(data.lastGymVisit);
+         }
+
+         if (data.exercises) {
+
+            data.exercises.forEach(exercise => {
+
+               console.log("Processing Exercise: ", exercise);
+               if (exercise && exercise.name) {
+                  const exerciseCard = createExerciseCard(exercise);
+                  exerciseList.appendChild(exerciseCard);
+               } else {
+                  console.log("Error Creating Exercise Card For: ", exercise);
+               }
+            });
+         } else {
+            console.log("Could not find exercises.");
+            exerciseList.innerHTML = '<p>No exercises found.</p>';
+         }
+      })
+      .catch(error => {
+         console.error('Error fetching session data:', error);
+         //exerciseList.innerHTML = '<p>Error loading exercises. Please try again later.</p>';
+      });
+});
+
+//logout button
+logOutButton.addEventListener('click', function (e) {
+   e.preventDefault();
+   console.log("Logout button clicked.");
+   fetch('/logout', {
+         method: 'GET',
+         cache: 'no-cache'
+      })
+      .then(response => {
+         if (response.redirected) {
+            window.location.href = response.url;
+         } else {
+            console.error('Logout failed, no redirect');
+         }
+      })
+      .catch(error => {
+         console.error('Error:', error);
+      });
+});
+
+//add exercise
+addExerciseButton.addEventListener('click', function(e) {
+   
+   //show the form
+   addExercisePopup.style.display = 'block';
+   addExerciseButton.classList.add('formBlur');
+   
+});
+
+closeExerciseForm.addEventListener('click', function(e) {
+   
+   //clear the form
+   addExerciseForm.reset();
+   //hide the form
+   addExercisePopup.style.display = 'none';
+   addExerciseButton.classList.remove('formBlur');
+   
+});
+
+addExerciseForm.addEventListener('submit', function(e) {
+   e.preventDefault();
+   const exerciseAddName = document.getElementById('exerciseName').value;
+   const exerciseAddWeight = document.getElementById('exerciseWeight').value;
+   fetch('/addExercise', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+         exerciseAddName: exerciseAddName,
+         exerciseAddWeight: exerciseAddWeight
+      })
+   })
+   .then(response => response.json())
+   .then(data => {
+      if (data.success) {
+         //debug log
+         console.log('Exercise added successfully');
+         //call our createExerciseCard to make a card for the new exercise
+         const exerciseCard = createExerciseCard(data.exercise);
+         //append it to our exercise list 
+         document.getElementById('exerciseList').appendChild(exerciseCard);
+         //hide the form
+         addExercisePopup.style.display = 'none';
+         //reset the form fields
+         addExerciseForm.reset();
+      } else {
+         console.log ('Failed to add exercise to profile.');
+      }
+   })
+   .catch(error => {
+      console.log("Error appending exercise: ", error);
+   });
+});
+
+//update date of last visited date to the gym
+updateGymVisitButton.addEventListener('click', function (e) {
+   e.preventDefault();
+   const newVisitDate = new Date();
+   //const savedInputElement = document.getElementById('savedInput');
+
+   fetch('/saveGymVisit', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+            newVisitDate: newVisitDate
+         })
+      })
+      .then(respone => respone.json())
+      .then(data => {
+         if (data.success) {
+            lastGymVisit.textContent = newVisitDate;
+         }
+      })
+      .catch(error => {
+         console.log("Error in saving gym visit input: ", error);
+      });
+});
---
+++ serverNEW.js
@@ -0,0 +1,405 @@
+//express JS setup
+const express = require('express');
+//session setup
+const session = require('express-session');
+//mongoDB session storage
+const MongoDBStore = require('connect-mongodb-session')(session);
+const mongoose = require('mongoose');
+//user schema 
+const User = require('./models/user.js');
+//express app
+const app = express();
+//file path
+const path = require('path');
+const user = require('./models/user.js');
+
+//dotenv file 
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   })
+   .then(() => {
+      console.log("Successfully connected to MongoDB Atlas");
+      console.log("Connected to DB: ", mongoose.connection.name);
+      console.log(mongoose.connection.collections);
+   })
+   .catch(err => console.log("Error: ", err));
+
+//mongoDBStore 
+
+const store = new MongoDBStore({
+   uri: process.env.MONGODB_URI,
+   collection: 'sessions',
+   expires: 1 * 60 * 60 * 1000 //1 hr
+});
+
+store.on('error', function (error) {
+   console.error('Session storage error: ', error);
+});
+
+
+//session parameters
+app.use(session({
+   secret: process.env.SESSION_SECRET, //delegate session secret to the env file 
+   cookie: {
+      maxAge: 1 * 60 * 60 * 1000
+   },
+   store: store,
+   resave: false,
+   saveUninitialized: false
+}));
+
+
+//tell Render service to select their own port, use 3000 otherwise
+const port = process.env.PORT || 3000;
+
+//hardcoded test creditials
+const testUsername = 'testUser';
+const testPassword = 'password123';
+
+//?: App to let express parse JSON bodies
+app.use(express.json());
+
+//we are returning files from the public directory only
+//tell Render to use public folder to serve static pages
+app.use(express.static((path.join(__dirname, 'public'))));
+
+
+//setup get handler for index.html for when server starts
+app.get('/', (req, res) => {
+   if (req.session.isAuthenticated) {
+      console.log("Already Authenticated: " , req.session, "Sending to Dashboard.");
+      res.redirect('/');
+   }
+   res.sendFile(__dirname + '/public/index.html');
+});
+
+//POST requests made to the /submit endpoint end up here 
+app.post('/submit', async (req, res) => {
+   //log what was sent to the post
+   console.log('LogIn form data received:', req.body);
+
+   //UNCHECKED: makes the first form entry the username and the second entry the password 
+   const {
+      username,
+      password
+   } = req.body;
+
+   try {
+      //log the variables.
+      console.log(`Received username: ${username}, password: ${password}`);
+      //look in db for user
+      const user = await User.findOne({
+         username,
+         password
+      });
+      console.log("User Found: ", user);
+      //we compare for valid login credentials
+      if (user) {
+         //log directory of valid login page
+         console.log(__dirname + '/public/dashboard.html');
+
+         //set session parameters 
+         req.session.userId = user._id;
+         req.session.username = username;
+         req.session.isAuthenticated = true;
+
+         //debug
+         console.log('Session created:', {
+            sessionId: req.sessionID,
+            userId: req.session.userId,
+            username: req.session.username,
+            isAuthenticated: req.session.isAuthenticated
+         });
+   
+         //save the session
+         req.session.save((err) => {
+            if (err) {
+               console.error("Error trying to save session: ", err);
+               return res.status(500).json({
+                  error: 'Failed to save session'
+               });
+            } else {
+               console.log("Saved session successfully.");
+               console.log("Session ID: " + req.sessionID);
+               res.json({
+                  success: true,
+                  redirect: '/dashboard'
+               });
+            }
+         })
+
+      } else {
+         res.json({
+            success: false,
+            redirect: '/invalidLogin'
+         });
+      }
+   } catch (error) {
+      console.error("Error Logging In: ", error);
+      res.status(500).json({
+         error: 'Failed to Log In'
+      });
+   }
+});
+
+//app get handler for dashbaord
+
+app.get('/dashboard',  async (req, res) => {
+   if (!req.session.isAuthenticated) {
+      return res.redirect('/');
+   }
+   try {
+      console.log('New Dashboard route hit:', {
+         sessionId: req.sessionID,
+         isAuthenticated: req.session.isAuthenticated,
+         userId: req.session.userId
+      });
+      //find user based on session ID
+      const user = await User.findOne({ _id: req.session.userId });
+      if (!user) {
+         console.log("User found: ", user.username, user._id);
+         req.session.destroy();
+         return res.redirect('/');
+      } else {
+         res.sendFile(path.join(__dirname, 'public', 'dashboardNew.html'));
+      }
+   } catch (error) {
+      console.error("Dashboard error:", error);
+      res.status(500).send('Server error');
+   }
+});
+
+//logout get handler
+app.get('/logout', (req, res) => {
+      console.log("Current session:", req.session); // Log the current session
+
+   req.session.destroy((err) => {
+      if (err) {
+         console.log("Error during session destruction:", err);
+      } else {
+         console.log("Session destroyed");
+      }
+
+      res.redirect('/logoutPage') // Serve logout page
+   });
+});
+
+app.get('/logoutPage', (req, res) => {
+   res.sendFile(path.join(__dirname, 'public', 'logout.html'));
+});
+
+//app get handler to retrieve session data 
+app.get('/sessionData', async (req, res) => {
+   if (req.session.isAuthenticated) {
+      try {
+         const user = await User.findOne({
+            username: req.session.username
+         });
+         res.json({
+            username: user.username,
+            exercises: user.exercises,
+            lastGymVisit: user.lastGymVisit
+         });
+      } catch (error) {
+         console.log("Error loading User Session Data: ", error);
+      }
+   } else {
+      res.json({
+         message: "NO AUTHORIZATION"
+      });
+   }
+});
+
+//handler for posting an input from user
+app.post('/saveGymVisit', async (req, res) => {
+   if (req.session.isAuthenticated) {
+      const {
+         newVisitDate
+      } = req.body;
+
+      console.log("req.body: ", newVisitDate);
+
+      await User.findOneAndUpdate({
+         username: req.session.username
+      }, {
+         lastGymVisit: newVisitDate
+      }, {
+         new: true,
+         runValidators: true
+      });
+      res.json({
+         success: true
+      });
+   } else {
+      res.status(403).json({
+         success: false,
+         message: 'Not authenticated'
+      }); //send error if no auth
+   }
+});
+
+app.post('/updateExercise', async (req, res) => {
+   if (req.session.isAuthenticated) {
+      const {
+         exerciseName,
+         weight
+      } = req.body;
+
+      console.log("req.body: ", req.body);
+
+      const user = await User.findOneAndUpdate({
+         username: req.session.username,
+         'exercises.name': exerciseName
+      }, {
+         $set: {
+            'exercises.$.lastWeight': weight,
+            'exercises.$.lastUpdated': new Date()
+         }
+      }, {
+         new: true,
+         runValidators: true
+      });
+
+      if (user) {
+
+         console.log("Updated User Exercises Profile: ", user.exercises);
+
+         res.json({
+            success: true,
+            message: 'Exercise weight updated successfully'
+         });
+      } else {
+         return res.status(404).json({
+            success: false,
+            message: 'User not found'
+         });
+      }
+   }
+});
+
+app.post('/addExercise', async (req, res) => {
+   if (req.session.isAuthenticated) {
+      const {
+         exerciseAddName,
+         exerciseAddWeight
+      } = req.body;
+      try {
+         const user = await User.findOneAndUpdate({
+            username: req.session.username
+         }, {
+            $push: {
+               exercises: {
+                  name: exerciseAddName,
+                  lastWeight: exerciseAddWeight,
+                  lastUpdated: new Date()
+               }
+            }
+         }, {
+            new: true,
+            runValidators: true
+         });
+
+         if (user) {
+            //create a new exercise object from the last exercise in the list, the newest added exercise
+            const newAddExercise = user.exercises[user.exercises.length - 1];
+            //our response is a json with these parameters 
+            res.json({
+               success: true,
+               message: 'Exercise added successfully',
+               exercise: newAddExercise
+            });
+         } else {
+            //if error we send our response as an error with a json
+            res.status(404).json({
+               success: false,
+               message: "User not found"
+            });
+         }
+      } catch (error) {
+         console.log("Error adding exercise to database: ", error);
+         res.status(500).json({
+            success: false,
+            message: 'Error adding exercise'
+         });
+      }
+   } else {
+      res.status(403).json({
+         success: false,
+         message: 'User Not authenticated'
+      });
+   }
+})
+
+app.post('/deleteExercise', async (req, res) => {
+   if (req.session.isAuthenticated) {
+      const {
+         exerciseDeleteName
+      } = req.body;
+      try {
+         const user = await User.updateOne({
+            username: req.session.username
+         }, {
+            $pull: {
+               exercises: {
+                  name: exerciseDeleteName
+               }
+            }
+         }, {
+            new: true,
+            runValidators: true
+         });
+
+         if (user) {
+            //create a new exercise object from the last exercise in the list, the newest added exercise
+            //our response is a json with these parameters 
+            res.json({
+               success: true,
+               message: 'Exercise ' + exerciseDeleteName + ' deleted successfully.'
+            });
+         } else {
+            //if error we send our response as an error with a json
+            res.status(404).json({
+               success: false,
+               message: "User not found"
+            });
+         }
+      } catch (error) {
+         console.log("Error deleting exercise from database: ", error);
+         res.status(500).json({
+            success: false,
+            message: 'Error deleting exercise'
+         });
+      }
+   } else {
+      res.status(403).json({
+         success: false,
+         message: 'User Not authenticated'
+      });
+   }
+})
+
+//get handler for valid login endpoint
+app.get('/validLogin', (req, res) => {
+   res.sendFile(__dirname + '/public/validLogin.html');
+});
+
+//get handler for invalid login endpoint
+app.get('/invalidLogin', (req, res) => {
+   res.sendFile(__dirname + '/public/invalidLogin.html');
+});
+
+app.get('/loadExercise', (req, res) => {
+
+});
+
+//when the server starts successfully we send this message to the server side console
+app.listen(port, () => {
+   //use url given by render
+   const host = process.env.RENDER_EXTERNAL_URL || 'http://localhost:' + port;
+   console.log('Server running at: ' + host);
+   console.log('Port: ' + port);
+   console.log('RENDER_EXTERNAL_URL: ' + (process.env.RENDER_EXTERNAL_URL || 'Not set'));
+});
